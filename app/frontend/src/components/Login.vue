cb32c1e5bb61ef3203632cc9ed7b9913f682dc45
赵汉卿 <2016011248@secoder.net>
2018-10-10 01:17:44 +0800

    #16 refining the login page

--- app/frontend/src/components/Login.vue
+++ app/frontend/src/components/Login.vue
@@ -1,21 +1,27 @@
 <template>
 	<div id="login">
 		<Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
-	        <FormItem prop="username">
-	            <Input type="text" v-model="formInline.username" placeholder="Username">
-	                <Icon type="ios-person-outline" slot="prepend"></Icon>
-	            </Input>
-	        </FormItem>
-	        <FormItem prop="password">
-	            <Input type="password" v-model="formInline.password" placeholder="Password">
-	                <Icon type="ios-lock-outline" slot="prepend"></Icon>
-	            </Input>
-	        </FormItem>
-	        <FormItem>
-	            <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
-	        </FormItem>
-    	</Form>
-    	<router-link to="/Register">Register</router-link>
+      <FormItem prop="username">
+        <Input type="text" v-model="formInline.username" name="username" placeholder="Username">
+          <Icon type="ios-person-outline" slot="prepend"></Icon>
+        </Input>
+      </FormItem>
+      <FormItem prop="password">
+        <Input type="password" v-model="formInline.password" name="password" placeholder="Password">
+          <Icon type="ios-lock-outline" slot="prepend"></Icon>
+        </Input>
+      </FormItem>
+			<FormItem prop="job">
+				<RadioGroup v-model="job">
+      		<Radio label="student"></Radio>
+      		<Radio label="teacher"></Radio>
+  			</RadioGroup>
+			</FormItem>
+      <FormItem>
+        <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
+      </FormItem>
+  	</Form>
+  	<router-link to="/Register">Register</router-link>
 	</div>
 </template>
 
@@ -23,52 +29,55 @@
 	import axios from 'axios';
 	import { mapState, mapActions } from 'vuex';
 
-    export default {
-        data () {
-            return {
-                formInline: {
-                    username: '',
-                    password: '',
-                    rpassword: '',
-                    mobile: '',
-                    verification: '',
-                },
-                ruleInline: {
-                    username: [
-                        { required: true, message: 'Please fill in the user name', trigger: 'blur' }
-                    ],
-                    password: [
-                        { required: true, message: 'Please fill in the password.', trigger: 'blur' },
-                        { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
-                    ]
-                },
-            }
+  export default {
+  	data () {
+      return {
+        formInline: {
+          username: '',
+          password: '',
+          rpassword: '',
+          mobile: '',
+          verification: '',
+					job: '',
         },
-        computed: {
-        	...mapState('account', ['status'])
+				job: 'student',
+        ruleInline: {
+          username: [
+            { required: true, message: 'Please fill in the user name', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: 'Please fill in the password.', trigger: 'blur' },
+            { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
+          ]
         },
-        methods: {
-        	...mapActions('account', ['login', 'logout']),
-            handleSubmit(name) {
-                this.$refs[name].validate((valid) => {
-                    if (valid) {
-                        this.$Message.success('Send to server!');
-                        const data = this.formInline;
-					    axios.post('/api/user/login', data).then((resp) => {
-					        if (resp.data.status === 'success') {
-					    	    this.set_login();
-					          	this.get_user_info();
-					        } else {
-					          	this.msg = `Status:${resp.data.status}`;
-					        }
-					    });
-                    } else {
-                        this.$Message.error('Fail!');
-                    }
-                })
-            }
-        }
-    };
+      }
+    },
+    computed: {
+    	...mapState('account', ['status'])
+    },
+    methods: {
+    	...mapActions('account', ['login', 'logout']),
+      handleSubmit(name) {
+      	this.$refs[name].validate((valid) => {
+          if (valid) {
+            this.$Message.success('Send to server!');
+						this.formInline['job'] = this.job;
+            const data = this.formInline;
+    				axios.post('/api/user/login', data).then((resp) => {
+			        if (resp.data.status === 'success') {
+		    	    	this.set_login();
+		          	this.get_user_info();
+			        } else {
+		          	this.$Message.error('用户名或密码错误');
+			        }
+				    });
+          } else {
+            this.$Message.error('用户名或密码格式不正确');
+          }
+        })
+      }
+    }
+  };
 </script>
 <style type="text/css">
 	#login {
--- app/frontend/src/components/Register.vue
+++ app/frontend/src/components/Register.vue
@@ -1,35 +1,41 @@
 <template>
 	<div id="register">
 		<Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
-	        <FormItem prop="username">
-	            <Input type="text" v-model="formInline.username" placeholder="Username">
-	                <Icon type="ios-person-outline" slot="prepend"></Icon>
-	            </Input>
-	        </FormItem>
-	        <FormItem prop="password">
-	            <Input type="password" v-model="formInline.password" placeholder="Password">
-	                <Icon type="ios-lock-outline" slot="prepend"></Icon>
-	            </Input>
-	        </FormItem>
-	        <FormItem prop="rpassword">
-	            <Input type="text" v-model="formInline.rpassword" placeholder="Password Again">
-	                <Icon type="ios-lock-outline" slot="prepend"></Icon>
-	            </Input>
-	        </FormItem>
-	        <FormItem prop="mobile">
-	            <Input type="password" v-model="formInline.mobile" placeholder="Phone number">
-	            </Input>
-	        </FormItem>
-	        <FormItem prop="verification">
-	            <Input type="password" v-model="formInline.verification" placeholder="verification">
-	            </Input>
-	        </FormItem>
-	        <FormItem>
-	            <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
-	            <Button @click="sendMsg('formInline')">发送验证码</Button>
-	        </FormItem>
-    	</Form>
-    	<router-link to="/login">Cancel</router-link>
+      <FormItem prop="username">
+        <Input type="text" v-model="formInline.username" placeholder="Username">
+          <Icon type="ios-person-outline" slot="prepend"></Icon>
+        </Input>
+      </FormItem>
+      <FormItem prop="password">
+        <Input type="password" v-model="formInline.password" placeholder="Password">
+          <Icon type="ios-lock-outline" slot="prepend"></Icon>
+        </Input>
+      </FormItem>
+      <FormItem prop="rpassword">
+        <Input type="text" v-model="formInline.rpassword" placeholder="Password Again">
+          <Icon type="ios-lock-outline" slot="prepend"></Icon>
+        </Input>
+      </FormItem>
+      <FormItem prop="mobile">
+        <Input type="password" v-model="formInline.mobile" placeholder="Phone number">
+        </Input>
+      </FormItem>
+      <FormItem prop="verification">
+        <Input type="password" v-model="formInline.verification" placeholder="verification">
+        </Input>
+      </FormItem>
+			<FormItem prop="job">
+				<RadioGroup v-model="job">
+      		<Radio label="student"></Radio>
+      		<Radio label="teacher"></Radio>
+  			</RadioGroup>
+			</FormItem>
+      <FormItem>
+        <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
+        <Button @click="sendMsg('formInline')">{{ msgBox }}</Button>
+      </FormItem>
+  	</Form>
+  	<router-link to="/login">Cancel</router-link>
 	</div>
 </template>
 
@@ -37,55 +43,60 @@
 	import axios from 'axios';
 	import { mapState, mapActions } from 'vuex';
 
-    export default {
-        data () {
-            return {
-                formInline: {
-                    username: '',
-                    password: '',
-                    rpassword: '',
-                    mobile: '',
-                    verification: '',
-                },
-                ruleInline: {
-                    username: [
-                        { required: true, message: 'Please fill in the user name', trigger: 'blur' }
-                    ],
-                    password: [
-                        { required: true, message: 'Please fill in the password.', trigger: 'blur' },
-                        { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
-                    ]
-                },
-            }
+  export default {
+    data () {
+      return {
+        formInline: {
+          username: '',
+          password: '',
+          rpassword: '',
+          mobile: '',
+          verification: '',
+					job:'',
         },
-        computed: {
-        	...mapState('account', ['status'])
+				msgBox:'发送验证码',
+				job:'student',
+        ruleInline: {
+          username: [
+            { required: true, message: 'Please fill in the user name', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: 'Please fill in the password.', trigger: 'blur' },
+            { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
+          ]
         },
-        methods: {
-        	...mapActions('account', ['login', 'logout']),
-            handleSubmit(name) {
-                this.$refs[name].validate((valid) => {
-                    if (valid) {
-                        this.$Message.success('Send to server!');
-                        const data = this.form;
-					    axios.post('/api/user/login', data).then((resp) => {
-					        if (resp.data.status === 'success') {
-					    	    this.set_login();
-					          	this.get_user_info();
-					        } else {
-					          	this.msg = `Status:${resp.data.status}`;
-					        }
-					    });
-                    } else {
-                        this.$Message.error('Fail!');
-                    }
-                })
-            },
-            sendMsg(name) {
-            	axios.get('/api/user/verification').then((resp) => {
-
-            	})
-            }
-        }
-    };
+      }
+    },
+    computed: {
+    	...mapState('account', ['status'])
+    },
+    methods: {
+  		...mapActions('account', ['login', 'logout']),
+      handleSubmit(name) {
+        this.$refs[name].validate((valid) => {
+          if (valid) {
+          	this.$Message.success('Send to server!');
+            const data = this.form;
+		    		axios.post('/api/user/login', data).then((resp) => {
+			        if (resp.data.status === 'success') {
+			    	    this.set_login();
+	          		this.get_user_info();
+			        } else {
+		          	this.msg = `Status:${resp.data.status}`;
+			        }
+			    	});
+          } else {
+            this.$Message.error('Fail!');
+          }
+        })
+      },
+      sendMsg(name) {
+      	axios.get('/api/user/verification').then((resp) => {
+					if (resp.data.status === 'success') {
+						this.msgBox = '验证码发送成功';
+					}
+      	})
+      }
+    }
+  };
 </script>
