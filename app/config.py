3909483137deb1f1d4fc08f16ab4d3662d458f74
xiaocj <xiaocj16@mails.tsinghua.edu.cn>
2018-10-11 00:56:18 +0800

    change config

--- app/config.py
+++ app/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root12345@mariadb.SitTillGraduation.secoder.local:3306/NBTV?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@mariadb.SitTillGraduation.secoder.local:3306/NBTV?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
