79f15d3e14cab7a63017ab5f9bb0b83dab757ef5
Adil <1298939544@qq.com>
2018-10-12 21:32:25 +0800

    Refactor #22:利用flask蓝图完成用户视图函数的重构

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -4,4 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+from .user import user as user_blueprint
+app.register_blueprint(user_blueprint, url_prefix='/api/user')
+
 from app import views, models
