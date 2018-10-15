573394e095b84f914bcb0a40980651648675b1ff
Adil <1298939544@qq.com>
2018-10-15 08:52:37 +0800

    Chore #27:修复ESLint分号使用错误

--- app/app/frontend/src/main.js
+++ app/app/frontend/src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 import Vuex from 'vuex'
-Vue.use(iView);
+Vue.use(iView)
 Vue.config.productionTip = false
 Vue.use(Vuex)
 new Vue({
@@ -14,7 +14,7 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-});
+})
 
 const store = new Vuex.Store({
   state: {
