bed963056a2aaf3df82b5aad220e960f2abbe9d3
jamgun <13011598178@qq.com>
2018-10-17 23:55:52 +0800

    Feat #44:完善导航栏

--- app/app/frontend/src/App.vue
+++ app/app/frontend/src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app">
-    <Menu mode="horizontal" active-name="1" class="nav">
-      <router-link to="/login">
-        <MenuItem name="1">
-          <Icon type="ios-paper" />
-          登录
-        </MenuItem>
-      </router-link>
+    <Menu mode="horizontal" :theme="theme1" active-name="active" class="nav">
+
       <router-link to="/list">
         <MenuItem name="2">
           <Icon type="ios-people" />
           教室列表
         </MenuItem>
       </router-link>
-      <MenuItem name="3" class="userInfo">
-        我的信息
-      </MenuItem>
+
       <router-link to="/mylist">
       <MenuItem name="4" class="mylist">
+        <Icon type="logo-youtube" />
         我加入的直播间
       </MenuItem>
+      </router-link>
+      <router-link to="/login">
+        <MenuItem name="1" style="float: right;" class="ilogin">
+          <Icon type="ios-contact-outline" />
+          登录
+        </MenuItem>
       </router-link>
     </Menu>
   </br>
@@ -33,7 +33,8 @@ export default {
   name: 'App',
   data () {
     return {
-      theme1: 'light'
+      theme1: 'light',
+      active:"",
     }
   }
 };
@@ -58,5 +59,8 @@ export default {
 }
   .mylist{
     margin-right: 20px;
+  }
+  .ilogin{
+    float:right;
   }
 </style>
