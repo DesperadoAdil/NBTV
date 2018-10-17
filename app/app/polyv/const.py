22cd00790cfc17632334eb85fec4e56e665e672d
xcjthu <xiaocj16@mails.tsinghua.edu.cn>
2018-10-17 20:19:12 +0800

    Feat #41:调用保利威视的API接口，实现了频道的删除、主持人姓名的修改、直播间名称的修改

--- app/app/polyv/const.py
+++ app/app/polyv/const.py
@@ -5,5 +5,8 @@ AppID = "f5a3j199z5"
 AppSecret = "922ecd15b3d34fb79b641e22fd3377ff"
 MaxViewer = 100
 
-CreateChannelUrl = "http://api.polyv.net/live/v2/channels/"
-GetChannelInfoUrl = "http://api.polyv.net/live/v2/channels/%d/get"
+CreateChannelUrl = "https://api.polyv.net/live/v2/channels/"
+GetChannelInfoUrl = "https://api.polyv.net/live/v2/channels/%s/get"
+DeleteChannelUrl = "https://api.polyv.net/live/v2/channels/%s/delete"
+ChangeChannelNameUrl = "https://api.polyv.net/live/v2/channels/%s/update"
+ChangeChannelAuthorUrl = "http://api.polyv.net/live/v2/channelSetting/%s/setPublisher"
