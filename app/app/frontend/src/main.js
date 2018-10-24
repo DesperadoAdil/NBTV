c1a6a4119b0c0d7475ba78024522ee336d31a5ac
赵汉卿 <2016011248@secoder.net>
2018-10-25 00:04:40 +0800

    Fix: 修复登陆之后需要刷新才能显示用户名的bug，修复显示bug

--- app/app/frontend/src/main.js
+++ app/app/frontend/src/main.js
@@ -1,18 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import iView from 'iview';
-import VueCookies from 'vue-cookies';
-import 'iview/dist/styles/iview.css';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import iView from 'iview'
+import VueCookies from 'vue-cookies'
+import 'iview/dist/styles/iview.css'
 
-Vue.use(iView);
-Vue.use(VueCookies);
+Vue.use(iView)
+Vue.use(VueCookies)
 Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-});
+})
