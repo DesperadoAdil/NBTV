1d34cce3fb37571de49bc8404ef6572580dea940
heyuxuan <secoder.net>
2018-10-27 00:21:03 +0800

    Chore Trying to use api

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -23,12 +23,12 @@
 
       -->
 
-      <MenuItem name="1" style="float:right;" @click.native="showinfo = true" type="primary">
-        <Icon v-if="LoginOrLogout === '登录'" type="ios-contact-outline" />
-        {{ LoginOrLogout }}
-        <Drawer title="User Information" v-model="showinfo" width="480">
+      <MenuItem name="1" style="float:right;" @click.native="showInfo = true" type="primary">
+        <Icon v-if="LoginOrLogout === '登录'" type="ios-contact-outline" />{{ LoginOrLogout }}
+        <Drawer title="User Information" v-model="showInfo" width="480">
           <Form>
-
+            <br>
+            <br>
             <!-- 用户名称 -->
             <Row :gutter="32">
               <Col span="18">
@@ -39,7 +39,8 @@
               <Col span="6">
               </Col>
             </Row>
-
+            <br>
+            <br>
             <!-- 用户身份 -->
             <Row :gutter="32">
               <Col span="18">
@@ -50,61 +51,85 @@
               <Col span="6">
               </Col>
             </Row>
-
+            <br>
+            <br>
             <!-- 用户手机号 -->
             <Row :gutter="32">
               <Col span="18">
-                <FormItem label="Mobile" label-position="left" label-width="80">
-                  {{userInfo.mobile}}
+                <FormItem label="Current Mobile" label-position="left" label-width="80">
+                  <Input type="text" v-model="userInfo.mobile" readonly></Input>
                 </FormItem>
               </Col>
               <Col span="6">
                 <FormItem>
-                  <Button @click="modalmobile = true">Edit</Button>
-                  <Modal
-                    v-model="modalmobile"
-                    title="Change your mobile"
-                    @on-ok="changemobile"
-                    @on-cancel="cancel">
-                    <Input label="old phone number" v-model="userInfo.mobile" readonly="true"></Input>
-                    <Input label="new phone number" v-model="newmobile" placeholder="enter your new mobile here"></Input>
-                    <Button>Send Verification</Button>
-                    <Input label="Verification" placeholder="enter verification here"></Input>
-                    <Button>Submit</Button>
-                  </Modal>
+                  <Button v-if="!showNewMobile" @click="changeMobile()">Edit</Button>
                 </FormItem>
               </Col>
             </Row>
 
-            <!-- 用户密码 -->
-            <Row :gutter="32" >
+            <!-- 用户的新手机号 -->
+            <Row :gutter="32" v-if="showNewMobile">
               <Col span="18">
-                <FormItem label="Password" label-position="left" label-width="80">
-                  {{userInfo.password}}
+                <FormItem label="New Mobile" label-position="left" label-width="80">
+                  <Input type="text" v-model="this.newMobile"></Input>
                 </FormItem>
               </Col>
               <Col span="6">
                 <FormItem>
-                  <Button @click="changepassword()">{{passButton}}</Button>
+                  <Button >Send</Button>
+                </FormItem>
+              </Col>
+            </Row>
+
+            <!-- 设置验证码 -->
+            <Row :gutter="32" v-if="showNewMobile">
+              <Col span="18">
+                <FormItem label="Verification" label-position="left" label-width="80">
+                  <Input v-model="verification" placeholder="Verification Code Here"></Input>
+                </FormItem>
+              </Col>
+              <Col span="6">
+                <FormItem>
+                  <Button >Verify</Button>
+                </FormItem>
+              </Col>
+            </Row>
+
+            <br>
+            <br>
+
+            <!-- 用户密码 -->
+            <Row :gutter="32" >
+              <Col span="18">
+                <FormItem label="Current Password" label-position="left" label-width="80">
+                  <Input type="text" v-model="newPassword"></Input>
+                </FormItem>
+              </Col>
+              <Col span="6">
+                <FormItem>
+                  <Button v-if="!showRpass" @click="changePassword()">Edit</Button>
                 </FormItem>
               </Col>
             </Row>
 
             <!-- 重复的用户密码 -->
-            <Row :gutter="32" v-if="showrpass">
+            <Row :gutter="32" v-if="showRpass">
               <Col span="18">
-                <FormItem label="Password" label-position="left" label-width="80">
-                  {{userInfo.password}}
+                <FormItem label="New Password" label-position="left" label-width="80">
+                  <Input type="text" v-model="this.newRpassword"></Input>
                 </FormItem>
               </Col>
               <Col span="6">
+                <FormItem>
+                  <Button @click="submitPassChange()">Apply</Button>
+                </FormItem>
               </Col>
             </Row>
 
           </Form>
 
           <div class="demo-drawer-footer">
-            <Button style="margin-right: 8px" @click="showinfo = false">Apply</Button>
+            <Button style="margin-right: 8px" @click="showInfo = false">Apply</Button>
             <Button type="primary" @click.native="logout">Log out</Button>
           </div>
         </Drawer>
@@ -120,11 +145,16 @@ export default {
   name: 'App',
   data () {
     return {
-      showinfo: false,
-      editmobile: false,
-      showrpass: false,
-      newmobile: '',
-      passButton: 'Edit',
+      // boolean to show or not
+      showInfo: false,
+      showRpass: false,
+      showNewMobile: false,
+      // mobile and password
+      newMobile: '',
+      newPassword: '******',
+      newRpassword: '',
+      verification: '',
+      // hanky's var
       theme1: 'light',
       active: '',
       userInfo: {
@@ -142,24 +172,22 @@ export default {
     this.showUserInfo()
   },
   methods: {
-    logout () {
-      this.LoginOrLogout = '登录'
-      this.userInfo = {
-        status: '',
-        username: '',
-        password: '',
-        mobile: '',
-        job: ''
+    submitPassChange () {
+      // use some api
+      input: { status : 'login/logout',
+        mobile : '',
+        old_password : '',
+        new_password : '',
+        job : 'student/teacher'
       }
-      this.$cookies.remove('user')
-      router.push('/Login')
     },
-    changemobile () {
+    changeMobile () {
       // need to use api
-      this.$Message.info('change mobile')
+      this.showNewMobile = true
     },
-    cancel () {
-      this.$Message.info('Cancel the change of password')
+    changePassword () {
+      this.showRpass = true
+      this.newPassword = ''
     },
     isTeacher () {
       return this.userInfo['job'] === 'teacher'
@@ -177,15 +205,17 @@ export default {
         router.push('/UserInfo')
       }
     },
-    changepassword () {
-      this.showrpass = true
-      if (this.passButton === 'Edit') {
-        this.passButton = 'Apply'
-      } else { // when equals apply
-        this.passButton = 'Edit'
-        this.showrpass = false
-        // add axios api and ...
+    logout () {
+      this.LoginOrLogout = '登录'
+      this.userInfo = {
+        status: '',
+        username: '',
+        password: '',
+        mobile: '',
+        job: ''
       }
+      this.$cookies.remove('user')
+      router.push('/Login')
     },
     showUserInfo () {
       if (this.$cookies.get('user') === null) {
