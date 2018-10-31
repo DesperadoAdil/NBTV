4e18b73e0393b483a6d935194dd476a4ef4882cc
肖朝军 <2016011302@secoder.net>
2018-10-31 14:52:13 +0000

    Test #74:不知道为啥，unit-test阶段没有运行test.py

--- app/test.py
+++ app/test.py
@@ -3,10 +3,8 @@ from app import app
 import unittest
 
 
-
-if __name__ == '__main__':
-    testdir = "app/test/"
-    discover = unittest.defaultTestLoader.discover(testdir, pattern='test_*.py')
-    runner=unittest.TextTestRunner()
-    runner.run(discover)
-    unittest.main()
+testdir = "app/test/"
+discover = unittest.defaultTestLoader.discover(testdir, pattern='test_*.py')
+runner=unittest.TextTestRunner()
+runner.run(discover)
+unittest.main()
