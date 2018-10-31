01081cafae471572e78bbbd067cb6484e166cf8a
肖朝军 <2016011302@secoder.net>
2018-10-31 16:02:53 +0000

    Fix #74:写错房间url

--- app/app/test/test_classroom.py
+++ app/app/test/test_classroom.py
@@ -10,7 +10,7 @@ class ClassroomTest(BaseTestCase):
 			"title": "adilnb",
 			"class_password": "121121",
 			"thumbnail": "/static/image/test.jpg",
-			"url": "123",
+			"url": "242544",
 		}
 
 	dataerror = {
@@ -19,7 +19,7 @@ class ClassroomTest(BaseTestCase):
 		"title": "adilnb",
 		"class_password": "121121",
 		"thumbnail": "/static/image/test.jpg",
-		"url": "123"
+		"url": "242544"
 	}
 
 	def test_add_classroom(self):
@@ -42,12 +42,12 @@ class ClassroomTest(BaseTestCase):
 
 
 	def test_updateclass(self):
-		self.data['old_url'] = "123"
+		self.data['old_url'] = "242544"
 		# 这是可以正确插入的结果
 		response = self.app.post('/api/classroom/update_class', data = json.dumps(self.data, ensure_ascii = False))
 		self.assertEquals(response.data.decode('utf8'), '{"status": "success"}')
 
-		self.dataerror['old_url'] = "123"
+		self.dataerror['old_url'] = "242544"
 		# 这是不可以正确插入的结果
 		response = self.app.post('/api/classroom/update_class', data = json.dumps(self.dataerror, ensure_ascii = False))
 		self.assertEquals(response.data.decode('utf8'), '{"status": "error:password wrong"}')
